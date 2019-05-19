047a6d80237713bd3ad5ae6ef9c86b9efd96bacb
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2019-05-19 16:29:39 +0200

    KEYCLOAK-10316 Given a user with a password, updating it with a password hashed credential representation leads to the presence of 2 passwords

--- server-spi-private/src/main/java/org/keycloak/models/utils/RepresentationToModel.java
+++ server-spi-private/src/main/java/org/keycloak/models/utils/RepresentationToModel.java
@@ -1681,6 +1681,7 @@ public class RepresentationToModel {
                 hashedCred.setPeriod(30);
             }
             hashedCred.setCreatedDate(cred.getCreatedDate());
+            session.userCredentialManager().disableCredentialType(realm, user, hashedCred.getType());
             session.userCredentialManager().createCredential(realm, user, hashedCred);
             UserCache userCache = session.userCache();
             if (userCache != null) {
